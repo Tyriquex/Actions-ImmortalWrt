0e7be3ebf6160323e05a05b4235581b02ee9f5d4
Tyriquex <100818164+Tyriquex@users.noreply.github.com>
2025-10-01 09:49:56 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -2,5 +2,5 @@
 # Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
 # git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
 # git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
-src-git thirdparty_luci https://github.com/coolsnowwolf/luci.git
-src-git thirdparty_packages https://github.com/coolsnowwolf/packages.git
+echo 'src-git thirdparty_luci https://github.com/coolsnowwolf/luci.git' >>feeds.conf.default
+echo 'src-git thirdparty_packages https://github.com/coolsnowwolf/packages.git' >>feeds.conf.default
